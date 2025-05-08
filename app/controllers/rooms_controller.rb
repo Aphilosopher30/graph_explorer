17b338fffdfcd39ae81f9d0d8f49b4b054485472
aphilosopher30 <ashafer1@fordham.edu>
2025-05-08 14:00:42 -0600

    add update and edit to conroller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,5 +6,24 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @adjacent_rooms = @room.portals
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to room_path(@room), notice: "Room successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description)
+  end
 end
