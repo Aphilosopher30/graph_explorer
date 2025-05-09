57dd10a357f239cc1a75914720c2e2c686da5e2c
aphilosopher30 <ashafer1@fordham.edu>
2025-05-09 09:47:57 -0600

    add floor_plan to Rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,8 @@ class Room
 
   property :name, type: String
   property :description, type: String
-
+  property :floor_plan, type: String, default: '[1, 1, 1], [1, 1, 1], [1, 1, 1]'
+  
   has_many :both, :portals, type: :PORTAL, model_class: :Room
 
 end
