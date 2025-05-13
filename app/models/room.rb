5cf5c8435e70c5fdee3efa299dd0a21c905508c9
aphilosopher30 <ashafer1@fordham.edu>
2025-05-13 13:34:22 -0600

    room: add method to get all relations associated with a room

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,9 @@ class Room
   property :floor_plan, type: String, default: '{"arrays": [[1, 1, 1], [1, 1, 1], [1, 1, 1]]}'
 
   has_many :both, :portals, model_class: :Room, rel_class: :Portal
+
+  def passage_ways()
+    # @portals = @room.portals.where(to_node: adjacent_room)
+    self.query_as(:room).match('(room)-[r]-(n)').pluck('r')
+  end
 end
