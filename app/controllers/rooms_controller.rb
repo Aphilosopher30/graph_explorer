4e8b54364de66031c6b06f4c89d284e2f87c6025
aphilosopher30 <ashafer1@fordham.edu>
2025-05-19 10:29:17 -0600

    add to controller ability create new portals

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,11 +16,27 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     floor_plan = JSON.parse(@room.floor_plan)
     @matrix = floor_plan["arrays"]
-
   end
 
   def update
     @room = Room.find(params[:id])
+
+    unknownRoom = Room.find("b1e07b5b-d339-4b7a-9a54-1081f038063f")
+
+    doorways = params["doorways"]
+
+    if doorways != ""
+      doorways.keys.each { | key |
+        new_portal = Portal.new(to_node: unknownRoom, from_node: @room)
+        doorway = doorways[key]
+
+        new_portal.kind = doorway["kind"]
+        new_portal.description = doorway["description"]
+        new_portal.locked = doorway["locked"]
+        new_portal.save
+      }
+    end
+
     if @room.update(room_params)
       redirect_to room_path(@room), notice: "Room successfully updated."
     else
@@ -30,8 +46,7 @@ class RoomsController < ApplicationController
 
   def new
     @room = Room.new
-    @matrix = [[1 , 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1]]
-
+    @matrix = [[1, 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1]]
   end
 
   def create
@@ -44,8 +59,11 @@ class RoomsController < ApplicationController
   end
 
   private
-
   def room_params
     params.require(:room).permit(:name, :description, :floor_plan)
   end
+
+  # def portal_params
+  #   params.require(:doorways).permit(:kind, :description, :locked)
+  # end
 end
