807c470563270ef7ad429357ad47fba1d361d312
aphilosopher30 <ashafer1@fordham.edu>
2025-06-05 17:17:46 -0600

    make method to get all portal_nodes connected to a room

--- app/models/room.rb
+++ app/models/room.rb
@@ -14,4 +14,11 @@ class Room
     # self.query_as(:room).match('(room)-[r]-(n)').pluck('r')
     self.query_as(:room).match('(room)-[r:PORTAL]-(n)').pluck('r')
   end
+
+  def mysterious_passage_ways()
+    # @portals = @room.portals.where(to_node: adjacent_room)
+    # self.query_as(:room).match('(room)-[r]-(n)').pluck('r')
+    self.query_as(:room).match('(room)-[r:UNKNOWNPASSAGE]-(n)').pluck('r')
+  end
+
 end
