467f5d88411643d755d58df132f9f9bbde4f0693
aphilosopher30 <ashafer1@fordham.edu>
2025-06-05 17:58:38 -0600

    add controllers that will create or assign rooms to dorways wwhen entered

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -8,4 +8,37 @@ class ExploreController < ApplicationController
     @matrix = [[1, 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1], [1 , 1, 1, 1]]
   end
 
+  def create_new_room
+    @portal_node = PortalNode.find(params[:id])
+
+    room = Room.new({name: params['name'],  description: params['description'], floor_plan: params['floor_plan'] })
+
+    new_portal = Portal.new(to_node: room, from_node: @portal_node.room,
+      kind: @portal_node.kind,
+      description: @portal_node.description,
+      locked: @portal_node.locked )
+    new_portal.save
+    @portal_node.destroy
+
+
+    if room.save
+      redirect_to room, notice: "Room was successfully created."
+    end
+  end
+
+  def connect_existing_room
+    @portal_node = PortalNode.find(params[:id])
+
+    @room = Room.find(params[:room_id])
+
+    new_portal = Portal.new(to_node: @room, from_node: @portal_node.room,
+      kind: @portal_node.kind,
+      description: @portal_node.description,
+      locked: @portal_node.locked )
+    new_portal.save
+    @portal_node.destroy
+
+
+    redirect_to @room, notice: "Room was successfully linked."
+  end
 end
